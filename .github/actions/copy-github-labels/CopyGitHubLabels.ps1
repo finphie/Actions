39145492a8e75f6867dfec9cb7935d9262b41e00
finphie <19237170+finphie@users.noreply.github.com>
2022-12-31 11:31:50 +0900

    ラベル関連

---
+++ .github/actions/copy-github-labels/CopyGitHubLabels.ps1
@@ -0,0 +1,19 @@
+param (
+    [Parameter(Mandatory)]
+    [string]$repositories,
+
+    [Parameter(Mandatory)]
+    [string]$sourceRepository
+)
+
+gh label list --repo $sourceRepository
+$repositoryList = $repositories -Split "[, `r`n]"
+
+Write-Output ''
+Write-Output 'Clone labels'
+
+foreach ($repository in $repositoryList)
+{
+    Write-Output $repository
+    gh label clone $sourceRepository --force --repo $repository
+}
