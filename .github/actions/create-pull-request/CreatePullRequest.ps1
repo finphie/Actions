14922e62da0efee9467bc8daada9edcf3a6f9297
finphie <19237170+finphie@users.noreply.github.com>
2022-12-31 18:13:40 +0900

    CreatePullRequest

---
+++ .github/actions/create-pull-request/CreatePullRequest.ps1
@@ -0,0 +1,36 @@
+﻿param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$commitMessage,
+
+    [string]$title = '',
+    [string]$branch = 'create-pull-request/',
+    [string]$body = '',
+    [string]$labels = ''
+)
+
+# GitHub Actionsで実行している場合
+if ($Env:GITHUB_ACTIONS)
+{
+    git config --local user.name 'github-actions[bot]'
+    git config --local user.email '41898282+github-actions[bot]@users.noreply.github.com'
+}
+
+if ($title -eq '')
+{
+    $title = $commitMessage
+}
+
+$date = Get-Date -Format 'yyyyMMddHHmmss'
+$branchName = "$branch$date"
+$labelList = $labels -Replace "[ `r`n]", ','
+
+git checkout -b $branchName
+git add .
+git commit -m $commitMessage
+git push --set-upstream origin $branchName
+
+gh pr create `
+    --title "$title" `
+    --label "$labelList" `
+    --body "$body"
