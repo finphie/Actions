53620f8422b2aa1633ad54b7dcf3f458454fd0e5
finphie <19237170+finphie@users.noreply.github.com>
2023-01-04 18:20:52 +0900

    Update PascalToKebab.ps1

--- .github/actions/pascal-to-kebab/PascalToKebab.ps1
+++ .github/actions/pascal-to-kebab/PascalToKebab.ps1
@@ -5,36 +5,61 @@ param (
     [string]$text
 )
 
-[string]$pascalText = $text.Trim()
-[string]$lowerText = $pascalText.ToLowerInvariant()
-
-Write-Verbose "Text: `"$text`""
-Write-Verbose "Length: $($text.Length)"
-Write-Verbose "PascalText: `"$pascalText`""
-Write-Verbose "LowerText: `"$lowerText`""
-
-[string]$kebabText = ''
-
-# 元の文字列と小文字にした文字列で比較を行い、異なる文字の場合は'-'をその文字の前に追加する。
-# 最初の一文字は、'-'を追加する必要がないのでスキップする。
-foreach ($value in [Linq.Enumerable]::Skip([Linq.Enumerable]::Zip($pascalText, $lowerText), 1))
+function PascalToKebab
 {
-    if ($value[0] -cne $value[1])
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        $text
+    )
+
+    [string]$pascalText = $text.Trim()
+    [string]$lowerText = $pascalText.ToLowerInvariant()
+
+    Write-Verbose "Text: `"$text`""
+    Write-Verbose "Length: $($text.Length)"
+    Write-Verbose "PascalText: `"$pascalText`""
+    Write-Verbose "LowerText: `"$lowerText`""
+
+    [string]$kebabText = ''
+
+    # 元の文字列と小文字にした文字列で比較を行い、異なる文字の場合は'-'をその文字の前に追加する。
+    # 最初の一文字は、'-'を追加する必要がないのでスキップする。
+    foreach ($value in [Linq.Enumerable]::Skip([Linq.Enumerable]::Zip($pascalText, $lowerText), 1))
     {
-        $kebabText += '-'
+        if ($value[0] -cne $value[1])
+        {
+            $kebabText += '-'
+        }
+
+        $kebabText += $value[1]
     }
 
-    $kebabText += $value[1]
+    # 最初の一文字を結合する。
+    $kebabText = $lowerText[0] + $kebabText
+
+    return $kebabText
 }
 
-# 最初の一文字を結合する。
-$kebabText = $lowerText[0] + $kebabText
-
-[string]$output = "text=$kebabText"
-Write-Verbose $output
-
-if ($Env:GITHUB_ACTIONS)
+function Get-GitHubOutput
 {
-    Write-Verbose 'Set GITHUB_OUTPUT'
-    $output | Out-File $Env:GITHUB_OUTPUT -Append
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        $text
+    )
+
+    [Collections.Specialized.OrderedDictionary]$outputs = [Ordered]@{
+        'text' = PascalToKebab $text
+    }
+
+    return $outputs
 }
+
+[string]$rootPath = Split-Path $PSScriptRoot
+. $rootPath/WriteGitHubOutput.ps1
+
+[Collections.Specialized.OrderedDictionary]$outputs = Get-GitHubOutput -Text $text
+Write-GitHubOutput -OutputList $outputs
