c920887c892601e7b55702c5936889b31dd0a342
finphie <19237170+finphie@users.noreply.github.com>
2023-01-04 15:34:29 +0900

    Update PascalToKebab.ps1

--- .github/actions/pascal-to-kebab/PascalToKebab.ps1
+++ .github/actions/pascal-to-kebab/PascalToKebab.ps1
@@ -5,9 +5,27 @@ param (
     [string]$text
 )
 
-[string]$result = [Regex]::Replace($text, '(?<=.)(?=[A-Z])', '-').ToLower() -Replace '-+', '-'
-[string]$output = "text=$result"
+[string]$pascalText = $text.Trim()
+[string]$lowerText = $pascalText.ToLowerInvariant()
 
+[string]$kebabText = ''
+
+# 元の文字列と小文字にした文字列で比較を行い、異なる文字の場合は'-'をその文字の前に追加する。
+# 最初の一文字は、'-'を追加する必要がないのでスキップする。
+foreach ($value in [Linq.Enumerable]::Skip([Linq.Enumerable]::Zip($pascalText, $lowerText), 1))
+{
+    if ($value[0] -cne $value[1])
+    {
+        $kebabText += '-'
+    }
+
+    $kebabText += $value[1]
+}
+
+# 最初の一文字を結合する。
+$kebabText = $lowerText[0] + $kebabText
+
+[string]$output = "text=$kebabText"
 Write-Verbose $output
 
 if ($Env:GITHUB_ACTIONS)
