b5b02da851420dce6fa3ddc1deb2fe69261bbc62
finphie <19237170+finphie@users.noreply.github.com>
2023-01-04 14:00:33 +0900

    PascalToKebab

---
+++ .github/actions/pascal-to-kebab/PascalToKebab.ps1
@@ -0,0 +1,17 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$text
+)
+
+[string]$result = [Regex]::Replace($text, '(?<=.)(?=[A-Z])', '-').ToLower() -Replace '-+', '-'
+[string]$output = "text=$result"
+
+Write-Verbose $output
+
+if ($Env:GITHUB_ACTIONS)
+{
+    Write-Verbose 'Set GITHUB_OUTPUT'
+    $output | Out-File $Env:GITHUB_OUTPUT -Append
+}
