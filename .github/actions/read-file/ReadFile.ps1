f6f3df38e1723f1e843bbd02d568f49e2accd374
finphie <19237170+finphie@users.noreply.github.com>
2023-01-28 12:20:42 +0900

    Read file

---
+++ .github/actions/read-file/ReadFile.ps1
@@ -0,0 +1,32 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [ValidateScript({ Test-Path $_ -PathType Leaf }, ErrorMessage='"{0}" does not exist.')]
+    [string]$filePath
+)
+
+[string]$rootPath = Split-Path $PSScriptRoot
+. $rootPath/WriteGitHubOutput.ps1
+
+function Get-GitHubOutput
+{
+    [CmdletBinding()]
+    [OutputType([Collections.Specialized.OrderedDictionary])]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$text
+    )
+
+    [Collections.Specialized.OrderedDictionary]$outputs = [Ordered]@{
+        'text' = $text
+    }
+
+    return $outputs
+}
+
+
+$text = Get-Content $filePath
+
+[Collections.Specialized.OrderedDictionary]$outputs = Get-GitHubOutput -Text $text
+Write-GitHubOutput -OutputList $outputs
