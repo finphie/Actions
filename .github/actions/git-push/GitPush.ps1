72922d6d1e26dc1bf4ac111942d243eed23a4e86
finphie <19237170+finphie@users.noreply.github.com>
2023-01-28 22:43:46 +0900

    git-push

---
+++ .github/actions/git-push/GitPush.ps1
@@ -0,0 +1,19 @@
+﻿[CmdletBinding(SupportsShouldProcess)]
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$commitMessage
+)
+
+[string]$rootPath = Split-Path $PSScriptRoot
+. $rootPath/GitCommand.ps1
+
+# 差分なしの場合、以降の処理をスキップして正常終了する。
+if (!(Test-Diff))
+{
+    Write-Verbose 'Skip'
+    exit
+}
+
+Set-GitConfig
+Invoke-GitCommitAndPush -CommitMessage $commitMessage
