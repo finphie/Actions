831452473f47f59965413b990d483227e67b4f64
finphie <19237170+finphie@users.noreply.github.com>
2023-01-31 22:33:30 +0900

    upload-release-assets (#32)

---
+++ .github/actions/upload-release-assets/UploadReleaseAssets.ps1
@@ -0,0 +1,19 @@
+﻿[CmdletBinding(SupportsShouldProcess)]
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$tag,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$files
+)
+
+[string]$rootPath = Split-Path $PSScriptRoot
+. $rootPath/Utility.ps1
+
+[string[]]$fileList = Get-List -Value $files | ForEach-Object { Get-Item $_ }
+
+gh release upload $tag $fileList
+
+exit
