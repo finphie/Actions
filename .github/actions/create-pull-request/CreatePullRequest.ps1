c9b8845959e9f099a518f337c95713f61c1f86c9
finphie <19237170+finphie@users.noreply.github.com>
2025-01-19 22:17:14 +0900

    GitHub CLIの不具合回避

--- .github/actions/create-pull-request/CreatePullRequest.ps1
+++ .github/actions/create-pull-request/CreatePullRequest.ps1
@@ -33,11 +33,13 @@ Write-Verbose 'Create pull request'
 [string]$labelList = $labels -eq '' ? '' : ((Get-List -Value $labels) -Join ',')
 Write-Verbose "Label: $labelList"
 
+# GitHub CLI v2.64.0で発生する不具合の回避のため、headを指定する。
+# v2.65.0で修正
 if ($labelList -eq '')
 {
-    gh pr create --fill
+    gh pr create --fill --head $(git branch --show-current)
 }
 else
 {
-    gh pr create --fill --label $labelList
+    gh pr create --fill --label $labelList --head $(git branch --show-current)
 }
